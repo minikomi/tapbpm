c8346024466577a53bece6901b0f34fbe0546aef
Adam Moore <nerdfunk@gmail.com>
2017-08-10 14:35:35 +0900

    Get ready for upload to chrome app store

--- background.js
+++ background.js
@@ -2,36 +2,49 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var currentBpm = [];
-var lastTime;
-var resetTimeout;
+var currentBpm, lastTime, clickCount, resetTimeout;
+var activeIcons = ["images/active1_48.png", "images/active2_48.png"];
 
-function updateIcon() {
+function resetToInit(){
+  currentBpm = [];
+  lastTime = undefined;
+  clickCount = 0;
+  chrome.browserAction.setBadgeText({text: ""});
+  chrome.browserAction.setIcon({path:"images/sleep_48.png"});
+}
+
+function handleTap(ev) {
   if (!lastTime) {
+    clickCount = 0;
     lastTime = new Date();
   } else {
+    clickCount = (clickCount + 1) % 2;
     var thisTime = new Date();
     var diff = thisTime - lastTime;
     var newBpm = (60 * 1000) / diff;
-    chrome.browserAction.setIcon({path:"icon2.png"});
     currentBpm.push(newBpm);
     if(currentBpm.length > 10) {
       currentBpm.shift();
     }
     var avg = 0;
-    currentBpm.forEach(function(v){avg += v});
+    currentBpm.forEach(function(v){avg += v;});
     avg = avg / currentBpm.length;
     chrome.browserAction.setBadgeText({text: ""+Math.floor(avg)});
-    clearTimeout(resetTimeout);
-    resetTimeout = setTimeout(function(){
-      currentBpm = [];
-      lastTime = undefined;
-      chrome.browserAction.setBadgeText({text: ""});
-      chrome.browserAction.setIcon({path:"icon1.png"});
-    }, 2000)
     lastTime = thisTime;
+    clearTimeout(resetTimeout);
   }
+
+  // set icon
+  chrome.browserAction.setIcon({path:activeIcons[clickCount]});
+
+  // reset timeout to fall back to base state
+  resetTimeout = setTimeout(resetToInit, 2000);
 }
 
-chrome.browserAction.setIcon({path:"icon1.png"});
-chrome.browserAction.onClicked.addListener(updateIcon);
+resetToInit();
+chrome.browserAction.onClicked.addListener(handleTap);
+chrome.commands.onCommand.addListener(function(command){
+  if(command == "tap-bpm") {
+    handleTap();
+  }
+});
